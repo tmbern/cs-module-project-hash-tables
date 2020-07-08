993ec5ca57fbeb510b2396f59aa71cbc2bbda885
tmbern <tmbern42@gmail.com>
2020-07-08 14:44:21 -0600

    updated expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -2,8 +2,27 @@
 
 
 def expensive_seq(x, y, z):
-    # Your code here
+    h = {}
 
+    def inner_expensive_seq(x, y, z):
+
+        first = (x-1, y+1, z)
+        second = (x-2, y+2, z*2)
+        third = (x-3, y+3, z*3)
+
+        if x <= 0:
+            return y + z
+        if x > 0:
+            if first not in h:
+                h[first] = inner_expensive_seq(x-1, y+1, z)
+            if second not in h:
+                h[second] = inner_expensive_seq(x-2, y+2, z*2)
+            if third not in h:
+                h[third] = inner_expensive_seq(x-3, y+3, z*3)
+            
+        return h[first] + h[second] + h[third]
+
+    return inner_expensive_seq(x, y, z)
 
 
 if __name__ == "__main__":
